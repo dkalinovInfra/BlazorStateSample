52b09fc15caee01e3f10d27e4ee709632fbf531c
Deyan Kalinov <dkalinov@infragistics.com>
2023-10-18 10:50:46 +0300

    Initial commit

---
+++ BlazorStateSample/Model/UserInformation.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorStateSample.Model;
+
+public class UserInformation
+{
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Age { get; set; }
+    
+    public string Interests { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Phone { get; set; }
+    
+}
